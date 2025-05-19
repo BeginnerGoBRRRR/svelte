8aeaeff21f8586ae2236d35f67d0f7c4eb78ccc6
Hoàng Hải Trường <zerodawn23022003@gmail.com>
2025-05-19 16:00:01 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="scss">
+    h1{
+        color: red;
+        font-size: functions.toRem(36);
+        @include breakpoint.up('md'){
+            color: green;
+        }
+        @include breakpoint.up('lg'){
+            color: blue;
+        }
+    }
+</style>
